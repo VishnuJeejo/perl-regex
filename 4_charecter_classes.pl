4e30303e55ecb7b32b407633d96870591fc509e3
VishnuJeejo <vishnujeejo@outlook.com>
2025-07-09 23:53:37 +0530

    added few if conditions to 4th file

--- 4_charecter_classes.pl
+++ 4_charecter_classes.pl
@@ -3,26 +3,47 @@
 
 $string = "test string to check charecter ylasses";
 
-print("\n\n\tProgram to check how charecter classes work\n\n");
+print("\n\tProgram to check how charecter classes work\n\n");
 #print("string = $string\n");
 
 if($string =~ /[cg]lasses/)
 {
-    print("\neither glasses or classes is contained in the string\n");
+    print("either glasses or classes is contained in the string\n");
 }
 else
 {
-    print("\n  neither glasses or classes is contained in the string\n");
+    print("neither glasses or classes is contained in the string\n");
 }
 
 $string = "item3 item1 item2";
 
 if($string =~ /item([123])/) # the () capture the pattern that was matched. This can be checked using $1.
 {
-    print("\nstring contains $1\n");
+    print("string contains $1\n");
 }
 else
 {
-    print("\nstring doesnot contains item1 2 or 3\n");
+    print("string doesnot contains item1 2 or 3\n");
 }
 
+#examples from the documentation for better understanding complex expressions
+
+$string = "this will contain ]like this/ and this^"; #including metacharecters into the string to get accustomed to the 
+                                                     #complex regexp's
+if($string =~ /this\//)
+{
+    print("hit\n");
+}
+else
+{
+    print("not hit\n");
+}
+
+if($string =~ /this([\/\^])/) #again adding () around the [] to capture the regex that was matched to $1
+{
+    print("hit $1\n");
+}
+else
+{
+    print("not hit\n");
+}
